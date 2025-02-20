8ca7264957ff9bd0f1d82ab71e921321cffd1f44
Aastha-520609 <77205408+Aastha-520609@users.noreply.github.com>
2025-02-20 22:03:42 +0530

    practiced constructor-injection

---
+++ DisplayStaffDetails/src/main/java/com/spring/app/Driver.java
@@ -0,0 +1,20 @@
+package com.spring.app;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Driver {
+	
+	public static Department loadStaffDetails()
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		Department department = (Department)context.getBean("departmentObj");
+		return department;
+	}
+
+	public static void main(String[] args) {
+		Department department = loadStaffDetails();
+		department.displayStaffDetails();
+	}
+
+}
